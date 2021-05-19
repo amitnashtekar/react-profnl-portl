0ebad3a2c61d25064618dfd332a959cb3397c363
Amit.Ashtekar <Amit.Ashtekar@canberra.edu.au>
2021-05-19 23:11:23 +1000

    initial commit

---
+++ profnl-portl/src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Login from "./components/Login";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
