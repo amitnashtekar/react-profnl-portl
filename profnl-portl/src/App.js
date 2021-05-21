9bfb073062fb1c6c2565d3bc3fbe4d0c929b1cc8
Amit.Ashtekar <Amit.Ashtekar@canberra.edu.au>
2021-05-21 16:39:03 +1000

    added images and icons

--- profnl-portl/src/App.js
+++ profnl-portl/src/App.js
@@ -1,14 +1,20 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from "./components/Login";
+import Home from "./components/Home";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
-          <Route path="/">
+          <Route exact path="/">
             <Login />
+          </Route>
+          <Route exact path="/home">
+            <Header></Header>
+            <Home></Home>
           </Route>
         </Switch>
       </Router>
